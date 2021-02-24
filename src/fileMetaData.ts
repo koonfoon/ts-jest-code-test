17899b4366abb5b4c28dba70a5a9e62aa9076830
koonfoon <koonfoon@gmail.com>
2021-02-24 14:13:42 +0800

    fileMetaData.ts

---
+++ src/fileMetaData.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import execa from 'execa';
+import path from 'path';
+
+dotenv.config();
+
+export default (fileLocation: string): boolean => {
+    try {
+        const javaMetaDataLocation = path.resolve('.', <string>process.env.JAVAMETADATALOCATION)
+        const fileLocationAbsolute = path.resolve('.', fileLocation);
+
+        execa.sync('java', ['-jar', 'fileMetaData.jar', fileLocationAbsolute], {
+            cwd: javaMetaDataLocation,
+            stdio: 'inherit',
+        });
+
+        return true;
+    }  catch (err) {
+        console.log(err.shortMessage);
+        console.log('information getter crash😑');
+        return false;
+    }
+}
