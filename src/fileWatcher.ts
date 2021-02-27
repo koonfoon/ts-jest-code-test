44eedc8e114eb8e83cfd7568e4b728b0b37032ff
koonfoon <koonfoon@gmail.com>
2021-02-27 14:49:54 +0800

    developing

--- src/fileWatcher.ts
+++ src/fileWatcher.ts
@@ -31,4 +31,33 @@ export const newFileAdded = (newFileLocationAbsolute: string): void => {
     // Get the file name
     const newFileName = path.basename(newFileLocationAbsolute).split('.')[0];
     const bewFileNameWithExtension = path.basename(newFileLocationAbsolute);
-}
+
+    // determine the file final location
+    const fileDir = path.join(<string>process.env.FILEFINALDES, newFileName);
+
+    // TODO: create the new directory for the new file with the file name
+
+    // TODO: move the new file to the newly created directory
+};
+
+const creaDir = (dirLocation: string): boolean | never => {
+    fse.mkdirSync(dirLocation);
+    // Check if the directory is created
+    if (fse.pathExistsSync(dirLocation)) {
+        console.log(`${dirLocation} was created`);
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const moveItem = (source: string, destination: string): boolean => {
+    moveFile.sync(source, destination);
+    // Check if item move successfull
+    if (fse.pathExistsSync(destination)) {
+        console.log(`Item successfully moved to ${destination}`);
+        return true;
+    } else {
+        return false;
+    }
+};
