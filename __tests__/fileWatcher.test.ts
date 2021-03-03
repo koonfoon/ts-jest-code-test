e709366bc5a09815bcf491de5da01129db0fbd9f
koonfoon <koonfoon@gmail.com>
2021-03-03 15:59:09 +0800

    making progress

--- __tests__/fileWatcher.test.ts
+++ __tests__/fileWatcher.test.ts
@@ -1,12 +1,14 @@
-import dotenv from 'dotenv/config';
-import * as fileWatcher from '../src/fileWatcher.ts';
-import fileMetaData from '../src/fileMetaData.ts';
+import dotenv from 'dotenv';
 import chokidar from 'chokidar';
 import mockFile from 'mock-fs';
 import fse from 'fs-extra';
 import path from 'path';
 import { mocked } from 'ts-jest/utils';
 import dirFree from 'directory-tree';
+
+// Self made module
+import * as fileWatcher from '../src/fileWatcher';
+import fileMetaData from '../src/fileMetaData';
 
 dotenv.config();
 
@@ -15,3 +17,16 @@ jest.mock('../src/fileMetaData', () => {
 });
 
 let watcher: chokidar.FSWatcher;
+
+beforeAll(() => {
+    watcher = fileWatcher.startFileWatcher(<string>process.env.WATCHINGDIR);
+    mockFile.restore();
+});
+beforeEach(() => {});
+afterEach(() => {});
+afterAll(() => {});
+
+// Check if watcher is working fine
+test('testing if return value is instance of chokidar', () => {
+    expect(watcher).toBeInstanceOf(chokidar.FSWatcher);
+});
