cd2a9b9c936aa5b14458712056aeb6d6e01e52ff
koonfoon <koonfoon@gmail.com>
2021-03-03 18:13:41 +0800

    developing test code

--- __tests__/fileWatcher.test.ts
+++ __tests__/fileWatcher.test.ts
@@ -4,11 +4,12 @@ import mockFile from 'mock-fs';
 import fse from 'fs-extra';
 import path from 'path';
 import { mocked } from 'ts-jest/utils';
-import dirFree from 'directory-tree';
+import dirTree from 'directory-tree';
 
 // Self made module
 import * as fileWatcher from '../src/fileWatcher';
 import fileMetaData from '../src/fileMetaData';
+import mock from 'mock-fs';
 
 dotenv.config();
 
@@ -24,10 +25,19 @@ beforeAll(() => {
     mocked(fileMetaData).mockClear();
 });
 beforeEach(() => {
+    mockFile.restore();
     mocked(fileMetaData).mockClear();
 });
-afterEach(() => {});
-afterAll(() => {});
+afterEach(() => {
+    mockFile.restore();
+    mocked(fileMetaData).mockClear();
+});
+afterAll(() => {
+    watcher.close().then(() => console.log('watcher closed'));
+    jest.resetAllMocks();
+    mockFile.restore();
+    mocked(fileMetaData).mockClear();
+});
 
 // Check if watcher is working fine
 test('testing if return value is instance of chokidar', () => {
@@ -39,7 +49,7 @@ test('check if function newFileAdded() is called', () => {
     const spynewFileAdded = jest.spyOn(fileWatcher, 'newFileAdded');
     spynewFileAdded.mockImplementation(() => console.log('newFileAdded() is called'));
 
-    watcher.emit('add', path.join(<string>process.env.WATCHINGDIR, 'file.txt'));
+    watcher.emit('add', path.join(<string>process.env.WATCHINGDIR, 'fileName.txt'));
 
     expect(spynewFileAdded).toBeCalled();
 
@@ -50,4 +60,24 @@ test('Testing the mocked fileMetaData()', () => {
     mocked(fileMetaData).mockReturnValueOnce(true);
 
     expect(mocked(fileMetaData('path'))).toBeTruthy();
+});
+
+test('check if FileAdded() function work correctly', () => {
+    // Mock file system
+    mockFile({
+        './watchedDir': mockFile.load('./watchedDir', { lazy: false }),
+    });
+
+    fse.ensureFileSync(path.join(<string>process.env.WATCHINGDIR));
+
+    mocked(fileMetaData).mockImplementation((): boolean => {
+        fse.ensureFileSync(path.join(<string>process.env.WATCHINGDIR, 'fileName', 'fileName.nfo'));
+        return true;
+    });
+
+    fileWatcher.newFileAdded('./watchedDir/fileName.txt');
+
+    // Get the mocked final destination directory items and console log it
+    const directoryTree = dirTree(path.join(<string>process.env.DOTROOM));
+    console.log(directoryTree);
 });
