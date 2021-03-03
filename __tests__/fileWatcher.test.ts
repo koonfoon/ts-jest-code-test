2024caa5066dbf15f5cd55849e621d084eb9417b
koonfoon <koonfoon@gmail.com>
2021-03-03 17:32:25 +0800

    added new test code

--- __tests__/fileWatcher.test.ts
+++ __tests__/fileWatcher.test.ts
@@ -21,12 +21,33 @@ let watcher: chokidar.FSWatcher;
 beforeAll(() => {
     watcher = fileWatcher.startFileWatcher(<string>process.env.WATCHINGDIR);
     mockFile.restore();
+    mocked(fileMetaData).mockClear();
 });
-beforeEach(() => {});
+beforeEach(() => {
+    mocked(fileMetaData).mockClear();
+});
 afterEach(() => {});
 afterAll(() => {});
 
 // Check if watcher is working fine
 test('testing if return value is instance of chokidar', () => {
     expect(watcher).toBeInstanceOf(chokidar.FSWatcher);
+});
+
+// Check if function newFileAdded() is being called
+test('check if function newFileAdded() is called', () => {
+    const spynewFileAdded = jest.spyOn(fileWatcher, 'newFileAdded');
+    spynewFileAdded.mockImplementation(() => console.log('newFileAdded() is called'));
+
+    watcher.emit('add', path.join(<string>process.env.WATCHINGDIR, 'file.txt'));
+
+    expect(spynewFileAdded).toBeCalled();
+
+    spynewFileAdded.mockRestore();
+});
+
+test('Testing the mocked fileMetaData()', () => {
+    mocked(fileMetaData).mockReturnValueOnce(true);
+
+    expect(mocked(fileMetaData('path'))).toBeTruthy();
 });
